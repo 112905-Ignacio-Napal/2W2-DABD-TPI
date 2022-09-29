e7ebdc3c91e52b208805dd303ed0483c7492c055
Ignacio Napal <ignacio.napal.contractor@gdevtools.com>
2022-09-29 19:57:55 -0300

    Se definen constantes para trabajr con las cartas

---
+++ src/app/constants/baraja.ts
@@ -0,0 +1,46 @@
+import { PaloEnum, EstadoCartaEnum } from '../interfaces/carta';
+
+const numerosExistentes = [
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+  'A',
+];
+
+const palosExistentes = Object.values(PaloEnum);
+
+export const cartas = palosExistentes
+  .map((palo) => {
+    return numerosExistentes.map((numero) => {
+      let valor: number[] = [];
+      if (isNaN(Number.parseInt(numero))) {
+        if (numero === 'A') {
+          valor.push(1);
+          valor.push(11);
+        }
+        if (numero === 'Q' || numero === 'J' || numero === 'K') {
+          valor.push(10);
+        }
+      } else {
+        valor.push(Number.parseInt(numero));
+      }
+
+      return {
+        numOLetra: numero,
+        palo: palo,
+        estado: EstadoCartaEnum.EN_JUEGO,
+        valor: valor,
+        url: `../../assets/${palo.toLowerCase()}_${numero.toLowerCase()}.png`,
+      };
+    });
+  })
+  .flat(1);
