ed368b0e6d06e4c23bfc5b840001819be478372b
Ignacio Napal <ignacio.napal.contractor@gdevtools.com>
2022-09-29 20:00:11 -0300

    Se agrega componente carta

---
+++ src/app/carta/carta.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Carta } from '../interfaces/carta';
+
+@Component({
+  selector: 'app-carta',
+  templateUrl: './carta.component.html',
+  styleUrls: ['./carta.component.css'],
+})
+export class CartaComponent implements OnInit {
+  @Input() carta: Carta = {} as Carta;
+  constructor() {}
+
+  numSequence(n: number): Array<number> {
+    return Array(n);
+  }
+  ngOnInit(): void {}
+}
