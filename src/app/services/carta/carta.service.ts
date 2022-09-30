2738dd5ccd39ee547dc51c70a602b300ee123dd8
Ignacio Napal <ignacio.napal.contractor@gdevtools.com>
2022-09-29 22:51:58 -0300

    Se agrega metodo para obtener carta en el service

--- src/app/services/carta/carta.service.ts
+++ src/app/services/carta/carta.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { cartas } from 'src/app/constants/baraja';
+import { Carta } from 'src/app/interfaces/carta';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartaService {
   constructor() {}
+
+  getCarta(): Carta {
+    return cartas[0];
+  }
 }
