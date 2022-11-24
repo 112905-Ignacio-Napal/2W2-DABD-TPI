04eb541994c2a69a52e89d5a50d2fbb65dc20105
Ignacio Napal <112905@tecnicatura.frc.utn.edu.ar>
2022-11-24 12:17:50 -0300

    Se crean primeros 2 reportes

---
+++ Back/src/main/java/com/adbd/Back/dto/VictoriasCroupierDTO.java
@@ -0,0 +1,13 @@
+package com.adbd.Back;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class VictoriasCroupierDTO {
+    private Long victorias;
+    private Long partidasJugadas;
+}
